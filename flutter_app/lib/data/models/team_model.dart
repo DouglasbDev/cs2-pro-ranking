bf748957d23061020898b4fa45c762c6975042d6
Douglas Biserra <douglas.beserra@hotmail.com>
2026-08-20 14:55:13 -0300

    Implement PlayerModel and TeamModel classes for player and team data management

---
+++ flutter_app/lib/data/models/team_model.dart
@@ -0,0 +1,52 @@
+class TeamModel {
+  const TeamModel({
+    required this.id,
+    required this.name,
+    this.country,
+    this.mapsPlayed,
+    this.wins,
+    this.draws,
+    this.losses,
+    this.totalKills,
+    this.totalDeaths,
+    this.roundsPlayed,
+    this.kdRatio,
+    this.kdDiff,
+    this.ratingOverall,
+    this.logoUrl,
+  });
+
+  final int id;
+  final String name;
+  final String? country;
+  final int? mapsPlayed;
+  final int? wins;
+  final int? draws;
+  final int? losses;
+  final int? totalKills;
+  final int? totalDeaths;
+  final int? roundsPlayed;
+  final double? kdRatio;
+  final int? kdDiff;
+  final double? ratingOverall;
+  final String? logoUrl;
+
+  factory TeamModel.fromMap(Map<String, Object?> map) {
+    return TeamModel(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      country: map['country'] as String?,
+      mapsPlayed: map['maps_played'] as int?,
+      wins: map['wins'] as int?,
+      draws: map['draws'] as int?,
+      losses: map['losses'] as int?,
+      totalKills: map['total_kills'] as int?,
+      totalDeaths: map['total_deaths'] as int?,
+      roundsPlayed: map['rounds_played'] as int?,
+      kdRatio: (map['kd_ratio'] as num?)?.toDouble(),
+      kdDiff: map['kd_diff'] as int?,
+      ratingOverall: (map['rating_overall'] as num?)?.toDouble(),
+      logoUrl: map['logo_url'] as String?,
+    );
+  }
+}
